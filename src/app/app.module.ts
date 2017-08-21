3f4ce4ce932f1fac691fc41cd9f642cdd6ccd97d
wei33107@gmail.com <wei33107@gmail.com>
2017-08-21 16:04:08 +0800

    2017082104

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,19 @@ import { DataManagerComponent } from './data-manager/data-manager.component';
 import { ChartComponent } from './chart/chart.component';
 import { AnalysisComponent } from './analysis/analysis.component';
 import {AppRoutingModule} from './app-routing.module';
-import {HttpModule} from '@angular/http';
+import {HttpModule, JsonpModule} from '@angular/http';
 import {AuthGuard} from './log-manager/auth-guard.service';
 import {AuthService} from './log-manager/auth.service';
+
+import { ChartModule } from 'angular2-highcharts';
+import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+
+export function highchartsFactory() {
+  const hc = require('highcharts/highstock');
+  const dd = require('highcharts/modules/exporting');
+  dd(hc);
+  return hc;
+}
 
 @NgModule({
   declarations: [
@@ -24,9 +34,14 @@ import {AuthService} from './log-manager/auth.service';
   imports: [
     HttpModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartModule,
+    JsonpModule
   ],
-  providers: [AuthGuard, AuthService],
+  providers: [AuthGuard, AuthService, {
+    provide: HighchartsStatic,
+    useFactory: highchartsFactory
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Jsonp} from '@angular/http';
 
 @Component({
   selector: 'app-chart',
@@ -6,10 +7,87 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
+  options;
 
-  constructor() { }
+  data = [
+    [1282521600000, 35.97, 36, 35.04, 35.11],
+    [1282608000000, 34.67, 34.71, 34.09, 34.28],
+    [1282694400000, 34.01, 34.86, 33.89, 34.7],
+    [1282780800000, 35.06, 35.11, 34.33, 34.33],
+    [1282867200000, 34.54, 34.66, 33.65, 34.52]
+  ];
+
+  constructor(jsonp: Jsonp) {
+      this.options = {
+        title : { text : '期貨歷史線圖' },
+        chart: {
+          width: 1100,
+          height: 350
+        },
+        rangeSelector : {
+          selected : 1,
+          buttons: [{
+            type: 'day',
+            count: 1,
+            text: '1d'
+          }, {
+            type: 'day',
+            count: 7,
+            text: '7d'
+          }, {
+            type: 'month',
+            count: 1,
+            text: '1m'
+          }, {
+            type: 'year',
+            count: 1,
+            text: '1y'
+          }, {
+            type: 'all',
+            text: 'All'
+          }]
+        },
+        series : [{
+          type : 'candlestick',
+          name : 'CandleSticks',
+          data : this.data,
+          tooltip: {
+            valueDecimals: 2
+          }
+        }, {
+          name: 'avg20',
+          type: 'spline',
+          data: [
+            [1282521600000, 33],
+            [1282608000000, 32],
+            [1282694400000, 31],
+            [1282780800000, 35],
+            [1282867200000, 36]
+          ],
+          tooltip: {
+            valueDecimals: 2
+          }
+        }, {
+          name: 'avg60',
+          type: 'spline',
+          data: [
+            [1282521600000, 35.97],
+            [1282608000000, 34.67],
+            [1282694400000, 34.01],
+            [1282780800000, 35.06],
+            [1282867200000, 34.54]
+          ],
+          tooltip: {
+            valueDecimals: 2
+          }
+        }]
+      };
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+
+  }
 }
